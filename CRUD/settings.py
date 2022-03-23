00a8180e2e023089f1cfbd3ae90b39a492fb929b
Sizan Mahmud <59968225+sizan378@users.noreply.github.com>
2022-03-23 10:14:34 +0600

    Update settings.py

--- CRUD/settings.py
+++ CRUD/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure--qjb(u#pdh7gtd=#9411tx%c6*mwx*$dsbam8x5137#iah$326
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = [*]
 
 
 # Application definition
